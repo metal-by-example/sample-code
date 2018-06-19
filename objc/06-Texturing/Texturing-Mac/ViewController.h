49d7a9ff30a19eed0c6c9454f556233e757d7b9c
Brent Gulanowski <bgulanowski@gmail.com>
2018-06-19 06:56:40 -0400

    Add macOS app target.

---
+++ objc/06-Texturing/Texturing-Mac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Texturing-Mac
+//
+//  Created by Brent Gulanowski on 2018-06-19.
+//  Copyright © 2018 Metal by Example. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ objc/06-Texturing/Texturing-Mac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Texturing-Mac
+//
+//  Created by Brent Gulanowski on 2018-06-19.
+//  Copyright © 2018 Metal by Example. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
