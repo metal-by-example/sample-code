546c78803b985eef14f2a5a5e2cafecda17b9c7a
Brent Gulanowski <bgulanowski@gmail.com>
2018-06-19 08:48:08 -0400

    Add macOS target for chapter 8 CubeMapping.

---
+++ objc/08-CubeMapping/CubeMapping-Mac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CubeMapping-Mac
+//
+//  Created by Brent Gulanowski on 2018-06-19.
+//  Copyright © 2018 Metal By Example. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
